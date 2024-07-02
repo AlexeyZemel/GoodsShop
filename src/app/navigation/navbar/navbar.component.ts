3413cf3cfff63d207a6b4268591395033ba715d9
Alexey Zemel <loyxa.zemel@gmail.com>
2024-07-02 13:03:42 +0400

    added navbar

---
+++ src/app/navigation/navbar/navbar.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import {RouterLink} from "@angular/router";
+import {ButtonComponent} from "./button/button.component";
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [
+    RouterLink, FormsModule, ButtonComponent
+  ],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+  submitForm(form: NgForm) {
+    // Ваша логика обработки формы здесь
+    console.log(form.value.textInput);
+  }
+}
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {},
+    colors: {
+      bg: "#FBF8EC",
+      current: "currentColor",
+      white: "#ffffff",
+      black: "#000000",
+      green: "#70C05B",
+      grey: "#606060",
+      orange: "#FF6633",
+      orange2: "#FF5520",
+      silver: "#414141",
+      "bubble-gum": "#ff77e9",
+      button: "#F3F2F1",
+      sr: "#F3F2F1"
+    },
+    fontFamily: {
+      rubik: ["Rubik", "sans-serif"],
+    },
+  },
+  plugins: [],
+}
